7db8e13ef03274376a98071ef8d52b87f1eeccbc
lianjie li <lilianjie@catr.cn>
2018-12-25 09:33:59 +0800

    111

---
+++ WebSoftTest/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebSoftTest.Startup))]
+namespace WebSoftTest
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
