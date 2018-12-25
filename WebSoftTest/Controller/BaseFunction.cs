919333510d401ccc66901841b855ae9c92b58627
lianjie li <lilianjie@catr.cn>
2018-12-25 10:26:45 +0800

    12

---
+++ WebSoftTest/Controller/BaseFunction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// BaseFunction 的摘要说明
+/// </summary>
+public class BaseFunction
+{
+    public BaseFunction()
+    {
+        //
+        // TODO: 在此处添加构造函数逻辑
+        //
+
+        public void Test()
+        {
+
+        }
+    }
+}
